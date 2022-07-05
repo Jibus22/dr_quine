9a300a017e0e9857e6f44fddeb4a691ba82a2948
Le Corre <jibeguitare@hotmail.fr>
2022-07-05 21:09:33 +0200

    ✨ feat(asm): add Colleen and Grace ASM

---
+++ Colleen/ASM/Colleen.s
@@ -0,0 +1,22 @@
+section .data
+msg db "section .data%2$cmsg db %3$c%1$s%3$c%2$csection .text%2$cglobal main%2$cextern printf%2$c;comment outside fn%2$cmain:%2$c;comment into fn%2$cpush rbp%2$cmov rbp, rsp%2$ccall fn%2$cmov rdi, msg%2$cmov rsi, msg%2$cmov rdx, 10%2$cmov rcx, 34%2$ccall printf%2$cmov rax, 0%2$cleave%2$cret%2$c%2$cfn:%2$cxor rcx,rcx%2$c"
+section .text
+global main
+extern printf
+;comment outside fn
+main:
+;comment into fn
+push rbp
+mov rbp, rsp
+call fn
+mov rdi, msg
+mov rsi, msg
+mov rdx, 10
+mov rcx, 34
+call printf
+mov rax, 0
+leave
+ret
+
+fn:
+xor rcx,rcx
---
+++ Grace/ASM/Grace.s
@@ -0,0 +1,34 @@
+%define FLAG 1101o
+%define MODE 644o
+%define SYS_OPEN 2
+section .data
+filename db "Grace_kid.s", 0
+msg db "%4$cdefine FLAG 1101o%2$c%4$cdefine MODE 644o%2$c%4$cdefine SYS_OPEN 2%2$csection .data%2$cfilename db %3$cGrace_kid.s%3$c, 0%2$cmsg db %3$c%1$s%3$c%2$c%4$cmacro my_main 0%2$csection .text%2$cglobal main%2$cextern dprintf%2$cmain:%2$cpush rbp%2$cmov rbp, rsp%2$cmov rdi, filename%2$cmov rsi, FLAG%2$cmov rdx, MODE%2$cmov rax, SYS_OPEN%2$csyscall%2$cmov rbx, rax%2$cmov rdi, rax%2$cmov rsi, msg%2$cmov rdx, msg%2$cmov rcx, 10%2$cmov r8, 34%2$cmov r9, 37%2$ccall dprintf%2$cmov rdi, rbx%2$cmov rax, 3%2$csyscall%2$cmov rax, 0%2$cleave%2$cret%2$c%4$cendmacro%2$cmy_main%2$c"
+%macro my_main 0
+section .text
+global main
+extern dprintf
+main:
+push rbp
+mov rbp, rsp
+mov rdi, filename
+mov rsi, FLAG
+mov rdx, MODE
+mov rax, SYS_OPEN
+syscall
+mov rbx, rax
+mov rdi, rax
+mov rsi, msg
+mov rdx, msg
+mov rcx, 10
+mov r8, 34
+mov r9, 37
+call dprintf
+mov rdi, rbx
+mov rax, 3
+syscall
+mov rax, 0
+leave
+ret
+%endmacro
+my_main
